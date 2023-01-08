19f1c908424eff27c2ce5042f87b9acc88d0248f
Chipper Conley <calumon0@gmail.com>
2023-01-07 20:55:41 -0500

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,91 @@
+let home = new Image();
+home.src = "InventoryHome.png";
+
+let ientry = new Image();
+ientry.src = "InventoryEntry.png";
+let isearch = new Image();
+isearch.src = "InventorySearch.png";
+let ireport = new Image();
+ireport.src = "InventoryReport.png";
+
+let fentry = new Image();
+fentry.src = "FrequenterEntry.png";
+let fsearch = new Image();
+fsearch.src = "FrequenterSearch.png";
+let freport = new Image();
+freport.src = "FrequenterReport.png";
+
+let dentry = new Image();
+dentry.src = "DistributionEntry.png";
+let dsearch = new Image();
+dsearch.src = "DistributionSearch.png";
+let dreport = new Image();
+dreport.src = "DistributionReport.png";
+
+let homecanvas = document.getElementById("home");
+let homecontext = homecanvas.getContext('2d');
+
+let ientrycanvas = document.getElementById("ientry");
+let ientrycontext = ientrycanvas.getContext('2d');
+let isearchcanvas = document.getElementById("isearch");
+let isearchcontext = isearchcanvas.getContext('2d');
+let ireportcanvas = document.getElementById("ireport");
+let ireportcontext = ireportcanvas.getContext('2d');
+
+let fentrycanvas = document.getElementById("fentry");
+let fentrycontext = fentrycanvas.getContext('2d');
+let fsearchcanvas = document.getElementById("fsearch");
+let fsearchcontext = fsearchcanvas.getContext('2d');
+let freportcanvas = document.getElementById("freport");
+let freportcontext = freportcanvas.getContext('2d');
+
+let dentrycanvas = document.getElementById("dentry");
+let dentrycontext = dentrycanvas.getContext('2d');
+let dsearchcanvas = document.getElementById("dsearch");
+let dsearchcontext = dsearchcanvas.getContext('2d');
+let dreportcanvas = document.getElementById("dreport");
+let dreportcontext = dreportcanvas.getContext('2d');
+
+homecanvas.width = "775";
+homecanvas.length = "275";
+
+ientrycanvas.width = "775";
+ientrycanvas.length = "275";
+isearchcanvas.width = "775";
+isearchcanvas.length = "275";
+ireportcanvas.width = "775"
+ireportcanvas.length = "275";
+
+fentrycanvas.width = "775";
+fentrycanvas.length = "275";
+fsearchcanvas.width = "775";
+fsearchcanvas.length = "275";
+freportcanvas.width = "775"
+freportcanvas.length = "275";
+
+dentrycanvas.width = "775";
+dentrycanvas.length = "275";
+dsearchcanvas.width = "775";
+dsearchcanvas.length = "275";
+dreportcanvas.width = "775"
+dreportcanvas.length = "275";
+
+window.onload=function(){
+  homecontext.drawImage(home,0,0,775,275);
+  //when saying .onload, you are basically setting someone when the onload functions happens.
+  ientrycontext.drawImage(ientry,0,0,775,275);
+  isearchcontext.drawImage(isearch,0,0,775,275);
+  ireportcontext.drawImage(ireport,0,0,775,275);
+
+  fentrycontext.drawImage(fentry,0,0,775,275);
+  fsearchcontext.drawImage(fsearch,0,0,775,275);
+  freportcontext.drawImage(freport,0,0,775,275);
+
+  dentrycontext.drawImage(dentry,0,0,775,275);
+  dsearchcontext.drawImage(dsearch,0,0,775,275);
+  dreportcontext.drawImage(dreport,0,0,775,275);
+};
+
+  btnScrolltoTop.addEventListener("click", function(){
+    window.scrollTo(0,0);
+  });
--- math.js
+++ math.js
@@ -1,33 +1,38 @@
-
-let equationcanvas = document.getElementById("equation");
-let equationcontext = equationcanvas.getContext('2d');
-
-equationcontext.width = "400";
-equationcontext.length = "400";
-
-equationcontext.moveTo(0,0);
-equationcontext.lineTo(200,100);
-equationcontext.stroke();
-/*
 let equation = new Image();
 equation.src = "MathQuizMain.png";
+let correct = new Image();
+correct.src = "MathQuizCorrect.png";
+let incorrect = new Image();
+incorrect.src = "MathQuizIncorrect.png";
+let results = new Image();
+results.src = "MathQuizResults.png";
 
 let equationcanvas = document.getElementById("equation");
 let equationcontext = equationcanvas.getContext('2d');
+let correctcanvas = document.getElementById("correct");
+let correctcontext = correctcanvas.getContext('2d');
+let incorrectcanvas = document.getElementById("incorrect");
+let incorrectcontext = incorrectcanvas.getContext('2d');
+let resultscanvas = document.getElementById("results");
+let resultscontext = resultscanvas.getContext('2d');
 
-equationcontext.pikacanvas.width = "300";
-equationcontext.pikacanvas.height = "500";
+equationcanvas.width = "200";
+equationcanvas.length = "400";
+correctcanvas.width = "200";
+correctcanvas.length = "400";
+incorrectcanvas.width = "200";
+incorrectcanvas.length = "400";
+resultscanvas.width = "200";
+resultscanvas.length = "400";
 
 window.onload=function(){
-  equationcontext.drawImage(equation,0,0,100,100);
+  equationcontext.drawImage(equation,0,0,200,400);
   //when saying .onload, you are basically setting someone when the onload functions happens.
+  correctcontext.drawImage(correct,0,0,200,400);
+  incorrectcontext.drawImage(incorrect,0,0,200,400);
+  resultscontext.drawImage(results,0,0,200,400);
 };
-//onload();
-*/
 
-if(btnScrolltoTop){
   btnScrolltoTop.addEventListener("click", function(){
     window.scrollTo(0,0);
   });
-}
-//make a method from this that can be used in multiple pages
